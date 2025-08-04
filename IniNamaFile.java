f429c63ddcc38aabec99b1edb9d2f8e6f9070bc3
kholifatululfa <kholifatul_yulsin_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:18:42 +0700

    Add files via upload

--- IniNamaFile.java
+++ IniNamaFile.java
@@ -1,5 +1,12 @@
 public class IniNamaFile {
     public static void main(String[] args) {
+        //main method
        System.out.println("Kholifatul Ulfa Yulsin");
+       System.out.println("SMK Telkom Malang");
+       System.out.println("MTS Sirojut Tholibiin");
+       System.out.println("Ali Muksin");
+       System.out.println("Yuliyah");
+       System.out.println("CEO yang sukses dunia dan akhirat");
+       System.out.println("Mendengarkan Music");
     }
 }
