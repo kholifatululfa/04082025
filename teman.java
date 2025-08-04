0a0d75e100812134401cbc16dcfd9cd08e25eb4d
kholifatululfa <kholifatul_yulsin_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:59:43 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,39 @@
+public class teman {
+    public static void main(String[] args) {
+        //variabel -> tipe_data nama_variabel = value
+       String nama = "Najwa Az Zahra";
+       int usia = 15;
+       String citacita = "Bos Muda";
+       System.out.println(nama);
+       System.out.println(usia + " tahun");
+       System.out.println(citacita);
+       
+       String nama2 = "Radisty Kirana Dhisa Aqila";
+       int usia2 = 15;
+       String citacita2 = "Software Engineer";
+       System.out.println(nama2);
+       System.out.println(usia2 + " tahun");
+       System.out.println(citacita2);
+
+       String nama3 = "Shabrina";
+       int usia3 = 16;
+       String citacita3 = "Bos Sukses";
+       System.out.println(nama3);
+       System.out.println(usia3 + " tahun");
+       System.out.println(citacita3);
+
+       String nama4 = "Tazkia Adila Affandi";
+       int usia4 = 15;
+       String citacita4 = "Graphic Design";
+       System.out.println(nama4);
+       System.out.println(usia4 + " tahun");
+       System.out.println(citacita4);
+
+       String nama5 = "Zian Mahdia Arina";
+       int usia5 = 16;
+       String citacita5 = "Pengusaha";
+       System.out.println(nama5);
+       System.out.println(usia5 + " tahun");
+       System.out.println(citacita5);
+    }
+}
